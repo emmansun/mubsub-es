ddc296eca5fd32fe2f795e5ccea5527396499e04
Emman <emman.sun@cargosmart.com>
2023-03-20 10:05:35 +0800

    support mongodb driver 5.x

--- test/connection.js
+++ test/connection.js
@@ -1,37 +1,38 @@
-var assert = require('assert');
-var mubsub = require('../lib/index');
-var helpers = require('./helpers');
+var assert = require("assert");
+var mubsub = require("../lib/index");
+var helpers = require("./helpers");
 
-describe('Connection', function () {
-    it('emits "error" event', function (done) {
-        mubsub('mongodb://localhost:6666/mubsub_tests', { serverSelectionTimeoutMS: 3000 }).on('error', function () {
-            done();
-        });
+describe("Connection", function () {
+  it('emits "error" event', function (done) {
+    mubsub("mongodb://localhost:6666/mubsub_tests", {
+      serverSelectionTimeoutMS: 3000,
+    }).on("error", function () {
+      done();
+    });
+  });
+
+  it('emits "connect" event', function (done) {
+    this.client = mubsub(helpers.uri);
+
+    this.client.on("connect", function (db) {
+      done();
+    });
+  });
+
+  it("states are correct", function (done) {
+    var self = this;
+
+    this.client = mubsub(helpers.uri);
+
+    this.client.on("connect", function () {
+      assert.equal(self.client.state, "connected");
+
+      self.client.close(function () {
+        assert.equal(self.client.state, "destroyed");
+        done();
+      });
     });
 
-    it('emits "connect" event', function (done) {
-        this.client = mubsub(helpers.uri);
-
-        this.client.on('connect', function (db) {
-            done();
-        });
-    });
-
-
-    it('states are correct', function (done) {
-        var self = this;
-        
-        this.client = mubsub(helpers.uri);
-
-        this.client.on('connect', function () {
-            assert.equal(self.client.state, 'connected');
-            
-            self.client.close();
-            assert.equal(self.client.state, 'destroyed');
-
-            done();
-        });
-
-        assert.equal(self.client.state, 'connecting');
-    });
+    assert.equal(self.client.state, "connecting");
+  });
 });
